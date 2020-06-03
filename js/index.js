2c6ef49bee9b8695cd1288759469bc6aff37345d
SoohaMagdy <sooha.magdy@gmail.com>
2020-06-03 22:41:14 +0200

    adding files

---
+++ js/index.js
@@ -0,0 +1,49 @@
+const instance = new TypeIt('#replaceStrings', {
+  strings: ['developer'],
+  //-- Other options...
+}).go();
+
+new TypeIt('#replaceStrings', {
+  strings: ["Developer", "Designer"],
+  speed: 50,
+  loop: true,
+  breakLines: false,
+  waitUntilVisible: true
+}).go();
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    nav: true,
+
+  });
+});
+
+$(document).ready(function () {
+  $('.counter').each(function () {
+    var $this = $(this),
+      countTo = $this.attr('data-count');
+    $({ countNum: $this.text() }).animate({
+      countNum: countTo
+    },
+      {
+        duration: 3000,
+        easing: 'linear',
+        step: function () {
+          $this.text(Math.floor(this.countNum));
+        },
+        complete: function () {
+          $this.text(this.countNum);
+        }
+      });
+  });
+});
+
+var swiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+  },
+});
+
+
